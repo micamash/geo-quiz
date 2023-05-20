2f2bbbcc5909f3e18feac6320376df0ab7d8ec1b
Ashley Mical <smashlandi@gmail.com>
2023-05-20 08:37:58 -0500

    generate project and create models

---
+++ src/main/java/com/projects/geoquiz/model/Question.java
@@ -0,0 +1,50 @@
+package com.projects.geoquiz.model;
+
+public class Question {
+
+    private int questionId;
+    private int topicId;
+    private String questionText;
+    private String imageName;
+
+    public Question(){}
+
+    public Question(int questionId, int topicId, String questionText, String imageName){
+        this.questionId = questionId;
+        this.topicId = topicId;
+        this.questionText = questionText;
+        this.imageName = imageName;
+    }
+
+    public int getQuestionId(){
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId){
+        this.questionId = questionId;
+    }
+
+    public int getTopicId(){
+        return topicId;
+    }
+
+    public void setTopicId(int topicId){
+        this.topicId = topicId;
+    }
+
+    public String getQuestionText(){
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText){
+        this.questionText = questionText;
+    }
+
+    public String getImageName(){
+        return imageName;
+    }
+
+    public void setImageName(String imageName){
+        this.imageName = imageName;
+    }
+}
---
+++ src/test/java/com/projects/geoquiz/GeoQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.geoquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
