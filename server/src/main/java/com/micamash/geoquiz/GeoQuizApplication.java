9ee203ed6ab27e0f121a51eff8bc399ba6077840
Ashley Mical <ashleymmical@gmail.com>
2023-07-21 16:36:49 -0500

    updated database and rearranged folders

---
+++ server/src/main/java/com/micamash/geoquiz/GeoQuizApplication.java
@@ -0,0 +1,13 @@
+package com.micamash.geoquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoQuizApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/micamash/geoquiz/GeoQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micamash.geoquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
