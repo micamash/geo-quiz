492eb675f34571002f6da16af52daab2e9610f98
Ashley Mical <ashleymmical@gmail.com>
2023-07-26 13:25:05 -0500

    update

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,14 +2,3 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-
-
-module.exports = {
-  configureWebpack: {
-    resolve: {
-      alias: {
-        'axios': 'axios/dist/axios.min.js'
-      }
-    }
-  }
-};
